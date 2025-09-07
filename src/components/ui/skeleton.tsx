5f8dd241cc70ad015477672b1f5e2a5c04fa152c
Denis Ronchese <denis.ronchese@gmail.com>
2025-09-07 20:08:50 +0200

    Skeleton integration

--- src/components/ui/skeleton.tsx
+++ src/components/ui/skeleton.tsx
@@ -1,18 +1,16 @@
-import { cn } from "@/lib/utils";
+import { cn } from "@/lib/utils"
 
-function Skeleton({
-  className,
-  ...props
-}: React.HTMLAttributes<HTMLDivElement>) {
+function Skeleton({ className, ...props }: React.ComponentProps<"div">) {
   return (
     <div
+      data-slot="skeleton"
       className={cn(
-        "relative overflow-hidden rounded-md bg-gray-100 before:absolute before:inset-0 before:-translate-x-full before:animate-[shimmer_2s_infinite] before:bg-gradient-to-r before:from-transparent before:via-white/60 before:to-transparent",
-        className
+        "animate-pulse rounded-base bg-secondary-background border-2 border-border",
+        className,
       )}
       {...props}
     />
-  );
+  )
 }
 
-export { Skeleton };
+export { Skeleton }
