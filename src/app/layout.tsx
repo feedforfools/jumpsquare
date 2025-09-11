81ef3b1d36bec0501d5a4511335ffdf16fa4804d
Denis Ronchese <denis.ronchese@gmail.com>
2025-09-11 13:48:59 +0200

    Add custom icon, manifest and cleanup public folder

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -23,6 +23,9 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en" suppressHydrationWarning>
+      <head>
+        <meta name="apple-mobile-web-app-title" content="Here's the Jump" />
+      </head>
       <body className={dmSans.className}>
         <ThemeProvider
           attribute="class"
